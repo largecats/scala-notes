61e8d7e9f6889f794c24f4d79c357312906b62e1
xiaolf <xiaolf@seagroup.com>
2020-11-27 15:08:07 +0800

    updated

--- AirForce/src/main/scala/InvarianceExample.scala
+++ AirForce/src/main/scala/InvarianceExample.scala
@@ -4,12 +4,12 @@ object InvarianceExample extends App {
   val u2 = new SpyAircraft("Lockheed U-2")
   val f22 = new FighterAircraft("Lockheed Martin F-22 Raptor")
 
-  val reconnaissanceDuty = new PatrolDuty[SpyAircraft](u2)
-  reconnaissanceDuty.print_flight_log()
+  val SpyPatrol = new Patrol[SpyAircraft](u2)
+  SpyPatrol.print_flight_log()
 
-  // if PatrolDuty were to be covariant in A, the following illegal operation would be possible:
-  //  val genericDuty: PatrolDuty[Aircraft] = reconnaissanceDuty
-  //  genericDuty.aircraftOnPatrolDuty = f22
-  //  val reconnaissanceAircraft: SpyAircraft = reconnaissanceDuty.aircraftOnPatrolDuty // the u-2 would become f22, which has severe implications in terms of international relations
+  // if Patrol were to be covariant in A, the following illegal operation would be possible:
+  //  val genericDuty: Patrol[Aircraft] = SpyPatrol
+  //  genericDuty.aircraftOnPatrol = f22
+  //  val SpyAircraft: SpyAircraft = SpyPatrol.aircraftOnPatrol // the u-2 would become f22, which has severe implications in terms of international relations
 
 }
--- AirForce/src/main/scala/PatrolDuty.scala
+++ AirForce/src/main/scala/PatrolDuty.scala
@@ -1,6 +1,6 @@
 package main.scala
 
-class PatrolDuty[A](aircraft: A) { // invariant in A
-  var aircraftOnPatrolDuty: A = aircraft // mutable, should be invariant
+class Patrol[A](aircraft: A) { // invariant in A
+  var aircraftOnPatrol: A = aircraft // mutable, should be invariant
   def print_flight_log(): Unit = println(s"A ${aircraft.getClass.getSimpleName} is on duty today")
 }
